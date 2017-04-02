26e4e4d8c50397082945a24d38f8ec64425806d5
MeiK <meik2333@gmail.com>
2017-04-02 10:00:30 +0800

    update

--- StepByStep/settings.py
+++ StepByStep/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
 
     'Org',
     'Step',
+    'WEB',
 )
 
 MIDDLEWARE_CLASSES = (
--- StepByStep/urls.py
+++ StepByStep/urls.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 from Org import views as Org_views
 from API import views as API_views
 from Step import views as Step_views
+from WEB import views as WEB_views
 
 urlpatterns = [
-    url(r'^$', Step_views.index, name='index'),
+    url(r'^$', WEB_views.index, name='index'),
+
+    url(r'^API$', Step_views.index, name='API'),
     url(r'^API/GetOrgList', API_views.GetOrgList, name='GetOrgList'), #获得Org列表
     url(r'^API/GetStepList', API_views.GetStepList, name='GetStepList'), #获得指定Org的Step列表
     url(r'^API/Login', Org_views.Login, name='Login'), #登录
