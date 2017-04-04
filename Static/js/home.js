a19957d45167ffbf489a1f48b4b15a2a78f573ec
MeiK <meik2333@gmail.com>
2017-04-04 09:49:25 +0800

    体验优化

--- Static/js/home.js
+++ Static/js/home.js
@@ -3,7 +3,7 @@ function setOrgList() {
     $("#thead").html('<th>编号</th><th>简称</th><th>组织名</th>');
     for (var i = 0; i < orgList['count']; i++) {
         $("#tbody").append('<tr><td>' + i + '</td><td>' + orgList['list'][i]['shortName'] +
-            '</td><td><a href="javascript:void(0)" onclick="setStepList(\'' +
+            '</td><td style="text-align: left"><a href="javascript:void(0)" onclick="setStepList(\'' +
             orgList['list'][i]['id'] + '\')">' +
             orgList['list'][i]['name'] + '</a></td></tr>');
     }
