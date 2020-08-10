499c4349ca699ae0f223f755a3c8d5610685fd1a
MeiK <meik2333@gmail.com>
2020-08-10 16:17:48 +0800

    update

--- app/util/__init__.py
+++
@@ -1,6 +0,0 @@
-#coding=utf-8
-from flask import Blueprint
-
-util = Blueprint('util', __name__)
-
-from . import views
--- run.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-from config import Config
-from app import create_app, db, login_manager, CSRFProtect
-
-app = create_app(Config)
-
-if __name__ == '__main__':
-    app.jinja_env.cache = None
-    app.run(debug=True, port=5000, host='0.0.0.0')
