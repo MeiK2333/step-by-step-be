d64894ccb4a300dba406cdf55e5f29beff8417cb
MeiK <meik2333@gmail.com>
2020-08-28 16:43:12 +0800

    Update

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   "type": "postgres",
-  "host": "localhost",
+  "host": process.env.POSTGRES_HOST || "localhost",
   "port": 5432,
   "username": "postgres",
   "password": "password",
