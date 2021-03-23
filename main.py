a6d00f58f814037184069595b841629a69c6596a
MeiK <meik2333@gmail.com>
2021-03-23 19:58:43 +0800

    优化了获取 step 提交的代码

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from datetime import datetime
+
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 
 from models.db import get_db
 from models.group import Group
-from models.step import Step
+from models.step import Step, get_step_solutions
 from models.step_problem import StepProblem
 from models.step_user import StepUser
-from models.user import get_step_solutions, User
+from models.user import User
+from schemas.enums import ResultEnum
 
 app = FastAPI()
 
@@ -65,16 +68,44 @@ def step_detail(step_id: int, db: Session = Depends(get_db)):
             }
         )
     usr = db.query(StepUser).filter_by(step=step).all()
+
+    # 获取该 step 的所有提交
+    solutions = get_step_solutions(step, db)
     users = []
     for step_usr in usr:
-        solutions = get_step_solutions(step_usr.user, step, db)
+        bind_users = step_usr.user.bind_users
+        user_all_solutions = []
+        user_step_solutions = {}
+        # 获取该用户所有绑定账号的提交
+        for bind_user in bind_users:
+            bind_user_id = bind_user.id
+            if bind_user_id in solutions:
+                user_all_solutions.extend(solutions[bind_user_id])
+        # 如果已经 AC，则后续不再处理
+        # 如果没有 AC，则取最新的状态
+        for solution in user_all_solutions:
+            if (
+                user_step_solutions.get(solution.problem_id, {}).get("result")
+                == "Accepted"
+            ):
+                continue
+            if solution.submitted_at:
+                date = solution.submitted_at.strftime("%Y-%m-%d")
+            else:
+                date = datetime.now().strftime("%Y-%m-%d")
+            user_step_solutions[solution.problem_id] = {
+                "result": "Accepted"
+                if solution.result == ResultEnum.Accepted
+                else "WrongAnswer",
+                "date": date,
+            }
         users.append(
             {
                 "id": step_usr.user.id,
                 "nickname": step_usr.nickname,
                 "username": step_usr.user.username,
                 "class": step_usr.clazz,
-                "solutions": solutions,
+                "solutions": user_step_solutions,
             }
         )
     data = {"problems": problems, "users": users, "name": step.name}
--- models/user.py
+++ models/user.py
@@ -1,15 +1,6 @@
-from datetime import datetime
-from typing import List
+from sqlalchemy import Column, Integer, String, Boolean
 
-from sqlalchemy import Column, Integer, String, select, Boolean
-from sqlalchemy.orm import Session
-
-from models.bind_user import BindUser
 from models.db import Base
-from models.problem import Problem
-from models.solution import Solution, ResultEnum
-from models.step import Step
-from models.step_problem import StepProblem
 
 
 class User(Base):
@@ -18,39 +9,3 @@ class User(Base):
     username = Column(String(length=64), unique=True)
 
     robot = Column(Boolean, default=False)
-
-
-def get_step_solutions(user: User, step: Step, db: Session):
-    # 查询该用户所有的绑定账号
-    bind_query = select(BindUser.id).where(BindUser.user == user)
-    problem_query = (
-        select(Problem.id).join(StepProblem).where(StepProblem.step == step)
-    )
-    # 查找该用户所有绑定账号中在指定 step 中存在的题目的提交
-    solutions: List[Solution] = (
-        db.query(Solution)
-        .filter(
-            Solution.bind_user_id.in_(bind_query), Solution.problem_id.in_(problem_query)
-        )
-        # 提交时间从久到新
-        .order_by(Solution.submitted_at)
-        .all()
-    )
-
-    resp = {}
-    # 如果已经 AC，则后续不再处理
-    # 如果没有 AC，则取最新的状态
-    for solution in solutions:
-        if resp.get(solution.problem_id, {}).get("result") == "Accepted":
-            continue
-        if solution.submitted_at:
-            date = solution.submitted_at.strftime("%Y-%m-%d")
-        else:
-            date = datetime.now().strftime("%Y-%m-%d")
-        resp[solution.problem_id] = {
-            "result": "Accepted"
-            if solution.result == ResultEnum.Accepted
-            else "WrongAnswer",
-            "date": date,
-        }
-    return resp
