a3a429a7bc5cba863f6f053d370625696bc0e2b7
MeiK <meik2333@gmail.com>
2021-03-26 14:56:58 +0800

    添加题目管理，更新用户管理

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List, Optional
 
 import aiohttp
 from fastapi import FastAPI, Depends
@@ -10,6 +11,7 @@ from logger import module_logger
 from models.bind_user import BindUser
 from models.db import get_db
 from models.group import Group
+from models.problem import Problem
 from models.source import Source
 from models.step import Step, get_step_solutions
 from models.step_problem import StepProblem
@@ -19,6 +21,7 @@ from schemas.auth import create_access_token, Auth, get_current_auth, get_curren
 from schemas.enums import ResultEnum
 from schemas.exception import SBSException, exception_handler
 
+module_logger("uvicorn")
 logger = module_logger("stepbystep")
 
 app = FastAPI()
@@ -328,7 +331,22 @@ def step_users(step_id: int, db: Session = Depends(get_db)):
                 }
             )
         resp.append(item)
+    return resp
 
+
+@app.get("/step/{step_id}/problems")
+def step_problems(step_id: int, db: Session = Depends(get_db)):
+    resp = []
+    for step_problem in (
+        db.query(StepProblem)
+        .filter_by(step_id=step_id)
+        .order_by(StepProblem.order)
+        .all()
+    ):
+        step_problem: StepProblem
+        problem = step_problem.problem
+        source = problem.source
+        resp.append(step_problem)
     return resp
 
 
@@ -341,8 +359,7 @@ def delete_step_user(
     _current_auth: Auth = Depends(get_current_admin),
 ):
     """ 从指定的计划中删除指定的用户，注意，这并不会修改已绑定的账号关系（只是从当前计划中移除） """
-    step_user = db.query(StepUser).get(step_user_id)
-    db.delete(step_user)
+    db.query(StepUser).filter_by(id=step_user_id).delete()
     db.commit()
     return {}
 
@@ -355,65 +372,156 @@ class NewStepUserRequest(BaseModel):
 
 
 @app.post("/group/{group_id}/step/{step_id}/user")
-def new_step(
+def new_step_user(
     group_id: int,
     step_id: int,
     request: NewStepUserRequest,
     db: Session = Depends(get_db),
     current_auth: Auth = Depends(get_current_admin),
 ):
-    # TODO: 此处的逻辑需要明确整理一下
+    """
+    为计划添加 step_user 与其对应的 user 的 bind_user
+    提交的请求中信息有：
+        1. step_user（step、class、nickname）
+        2. bind_user（source、username）
+    我们需要做的是：为该 step_user 找到对应的 user，并为该 user 添加 bind_user 的绑定
+    找对应 user 的顺序如下
+    1. 如果 step_user 已存在，则 user 为 step_user.user
+    2. 如果 step_user 不存在
+        1. 如果 bind_user 存在，则 user 为 bind_user.user
+        2. 无 user，需要自动生成
+
+
+    1. 首先查找 user
+        1. 如果同一个 step_user（step、class、nickname 都相同，认为是同一个 step_user）存在，则 user 为 step_user.user
+        2. 如果该 step_user 不存在
+            1. 查找 bind_user 是否已经存在（source、username 相同）
+                1. 如果 bind_user 存在，则 user 为 bind_user.user
+                2. 如果 bind_user 不存在，则创建一个新的 user
+    2. 添加 step_user 与 user 的绑定，step_user.user = user
+    3. 使用信息中的 source 与 username 查找 bind_user
+        1. 如果 bind_user 不存在，则创建它
+    4. 添加 user 与 bind_user 的关联，注意一个 bind_user 只能被一个 user 关联，即若该 bind_user 之前已经与其他 user 绑定，则此操作失败
+    """
+    # 检查参数
     group = db.query(Group).get(group_id)
     step = db.query(Step).get(step_id)
     if step.group != group:
         raise SBSException(errmsg="how dare you!")
+    source = db.query(Source).filter_by(name=request.source).first()
+    if not source:
+        raise SBSException(errmsg="Source 不存在！")
+
     step_user: StepUser = (
         db.query(StepUser)
         .filter_by(nickname=request.nickname, clazz=request.clazz, step=step)
         .first()
     )
-    source = db.query(Source).filter_by(name=request.source).first()
-    if not source:
-        raise SBSException(errmsg="Source 不存在！")
+    bind_user: BindUser = (
+        db.query(BindUser).filter_by(source=source, username=request.username).first()
+    )
+
+    # 首先查找 User
     if step_user:
-        # 如果该计划已有该用户
-        # 如果与之前的 source 不同，则为该用户添加一条 bind
-        # 否则直接报错
-        for bind_user in step_user.user.bind_users:
-            bind_user: BindUser
-            if bind_user.source == source:
-                raise SBSException(errmsg="该用户已存在")
-        # 如果该绑定不存在，则创建
-        bind_user = db.query(BindUser).filter_by(source=source, username=request.username).first()
-        if not bind_user:
-            bind_user = BindUser()
-            bind_user.source = source
-            bind_user.username = request.username
-        bind_user.user = step_user.user
-        db.add(bind_user)
-        db.commit()
-        return {}
-    # 管理员导入的账号会自动创建一个用户
-    username = source.name + "-" + request.username
-    user = db.query(User).filter_by(username=username).first()
-    if not user:
-        user = User()
-        user.robot = True
-        user.username = username
-        db.add(user)
-        db.commit()
+        user = step_user.user
+    else:
+        if bind_user:
+            user = bind_user.user
+        else:
+            # 创建新的通过后台导入的 User
+            import_username = f"{source.name}-{step.id}-{request.username}"
+            user = db.query(User).filter_by(username=import_username).first()
+            if not user:
+                user = User()
+                user.username = import_username
+                user.robot = True
+                db.add(user)
+                db.commit()
+
+    # 然后为 User 添加 bind_user 绑定
+    if bind_user:
+        # 如果已有 bind_user 且该 bind_user 已经绑定了其他人
+        if bind_user.user != user:
+            raise SBSException(
+                errmsg=f"账号 {request.username} 已经被 {bind_user.user.username} 绑定，导入失败！"
+            )
+        else:  # 此时该账号已有一个相同的 bind_user，无需进行绑定操作
+            pass
+    else:  # 如果没有 bind_user，则创建
         bind_user = BindUser()
         bind_user.source = source
-        bind_user.username = request.username
         bind_user.user = user
+        bind_user.username = request.username
         db.add(bind_user)
         db.commit()
 
-    step_user = StepUser()
-    step_user.user = user
-    step_user.nickname = request.nickname
-    step_user.clazz = request.clazz
-    step_user.step = step
-    db.add(step_user)
+    # 最后为该 User 添加当前 step 的 step_user 绑定
+    if step_user:
+        # 如果已有 step_user 且该 step_user 已经绑定了其他人
+        if step_user.user != user:
+            raise SBSException(errmsg=f"理论上这个错误永远不会出现，如果你见到这个错误，请联系管理员～")
+        else:  # 无需处理
+            pass
+    else:  # 如果没有 step_user，则创建
+        step_user = StepUser(
+            nickname=request.nickname, clazz=request.clazz, step=step, user=user
+        )
+        db.add(step_user)
+        db.commit()
+
+    return step_user
+
+
+class EditStepProblemRequest(BaseModel):
+    project: Optional[str]
+    topic: Optional[str]
+    problem: str
+    source: str
+
+
+@app.put("/group/{group_id}/step/{step_id}/problems")
+def edit_step_problems(
+    group_id: int,
+    step_id: int,
+    request: List[EditStepProblemRequest],
+    db: Session = Depends(get_db),
+    current_auth: Auth = Depends(get_current_admin),
+):
+    """
+    为计划添加题目，将会全量覆盖之前的数据
+    如果题目还不存在，则打印警告并创建该题目
+    """
+    # 检查参数
+    group = db.query(Group).get(group_id)
+    step = db.query(Step).get(step_id)
+    if step.group != group:
+        raise SBSException(errmsg="how dare you!")
+    problems = []
+    for item in request:
+        source = db.query(Source).filter_by(name=item.source).first()
+        if not source:
+            raise SBSException(errmsg="Source not found!")
+        problem: Problem = db.query(Problem).filter_by(source=source, problem_id=item.problem).first()
+        if not problem:
+            logger.warning(f'未知的题目！{item.source} - {item.problem}')
+            problem = Problem()
+            problem.source = source
+            problem.problem_id = item.problem
+            db.add(problem)
+            db.commit()
+        problems.append(problem)
+
+    # 首先清除该计划之前的题目
+    db.query(StepProblem).filter_by(step=step).delete()
     db.commit()
+
+    for idx, problem in enumerate(problems):
+        step_problem = StepProblem()
+        step_problem.order = idx
+        step_problem.step = step
+        step_problem.problem = problem
+        step_problem.project = request[idx].project
+        step_problem.topic = request[idx].topic
+        db.add(step_problem)
+        db.commit()
     return {}
--- models/bind_user.py
+++ models/bind_user.py
@@ -1,15 +1,34 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint, text
 from sqlalchemy.orm import relationship
 
 from models.db import Base
 from models.source import Source
 
 
+def bind_user_link(content):
+    engine = content.engine
+    query = engine.execute(
+        text("select * from sources where id = :source_id"),
+        {"source_id": content.get_current_parameters()["source_id"]},
+    )
+    name = ''
+    for cur in query:
+        name = cur.name
+    username = content.get_current_parameters()["username"]
+    if name == "SDUT":
+        return f"https://acm.sdut.edu.cn/onlinejudge3/users?username={username}"
+    elif name == "POJ":
+        return f"http://poj.org/userstatus?user_id={username}"
+    elif name == "VJ":
+        return f"https://vjudge.net/user/{username}"
+    return ""
+
+
 class BindUser(Base):
     __tablename__ = "bind_user"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(length=64), index=True)
-    link = Column(String(length=256))
+    link = Column(String(length=256), default=bind_user_link)
 
     last_spider = Column(Integer, default=0)
 
