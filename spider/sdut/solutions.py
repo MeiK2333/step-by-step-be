3320f0293efb41d5ae29d80166df3327207e4458
MeiK <meik2333@gmail.com>
2022-07-20 11:38:39 +0800

    忽略比赛的提交

--- spider/sdut/solutions.py
+++ spider/sdut/solutions.py
@@ -66,6 +66,8 @@ def get_solution(user_id: int, start: int, session: requests.Session):
             if res == ResultEnum.Unknown:  # 如果遇到未知的数据，先跳过
                 logger.warning(f"crawl result {res}, continue!")
                 continue
+            if row.get('competition') is not None:  # 忽略比赛的提交
+                continue
             result.append(row)
             start = max(row["solutionId"], start)
     return result
